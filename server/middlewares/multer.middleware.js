a08c26a80cb7f5d00f297cc5615b853713a30808
Saif Rajpoot <saif.itdeveloper@gmail.com>
2026-03-12 23:11:56 +0500

    File Upload issue solved

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,15 +1,39 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "./temp/media")
+//     },
+//     filename: function (req, file, cb) {
+      
+//       cb(null, file.originalname)
+//     }
+//   })
+  
+// export const upload = multer({ 
+//     storage, 
+// })
+
 import multer from "multer";
+import fs from "fs";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./media")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+  destination: function (req, file, cb) {
+    const dir = "./temp/media";
+    
+    // Check if directory exists, if not, create it
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
     }
-  })
-  
+    
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    // Note: Using originalname can lead to overwrites if two users upload "image.png"
+    cb(null, file.originalname);
+  }
+});
+
 export const upload = multer({ 
-    storage, 
-})
+  storage, 
+});
